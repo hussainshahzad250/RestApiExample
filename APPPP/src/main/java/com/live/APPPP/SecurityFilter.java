6039f1911c931755809dd6b276b0aca5a2a8a4b7
Shahzad <Shahzad@192.168.0.5>
2016-11-23 01:05:09 +0530

    COmmit First Time

---
+++ APPPP/src/main/java/com/live/APPPP/SecurityFilter.java
@@ -0,0 +1,43 @@
+package com.live.APPPP;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+import org.glassfish.jersey.internal.util.Base64;
+
+@Provider
+public class SecurityFilter implements ContainerRequestFilter {
+
+	private static final String AUTHORIZATION_HEADER_KEY = "Authorization";
+	private static final String AUTHORIZATION_HEADER_PRIFIX = "Basic ";
+	private static final String SECURED_URL_PRIFIX = "secured";
+
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+
+		if (requestContext.getUriInfo().getPath().contains(SECURED_URL_PRIFIX)) {
+			List<String> authHeader = requestContext.getHeaders().get(AUTHORIZATION_HEADER_KEY);
+			if (authHeader != null && authHeader.size() > 0) {
+				String authToken = authHeader.get(0);
+				authToken = authToken.replaceFirst(AUTHORIZATION_HEADER_PRIFIX, "");
+				String string = Base64.decodeAsString(authToken);
+				StringTokenizer stringTokenizer = new StringTokenizer(string, ":");
+				String username = stringTokenizer.nextToken();
+				String password = stringTokenizer.nextToken();
+				if ("user".equals(username) && "password".equals(password)) {
+					return;
+				}
+			}
+			Response unauthorized = Response.status(Response.Status.UNAUTHORIZED)
+					.entity("You are not eligible to access the resource").build();
+			requestContext.abortWith(unauthorized);
+		}
+	}
+
+}
