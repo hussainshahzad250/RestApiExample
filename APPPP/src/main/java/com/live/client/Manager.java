9a678b38aa634bd503818ef8f9dd2dc8086bb568
Shahzad <Shahzad@192.168.0.7>
2016-11-24 00:51:27 +0530

    Commit

--- APPPP/src/main/java/com/live/client/Manager.java
+++ APPPP/src/main/java/com/live/client/Manager.java
@@ -2,6 +2,8 @@ package com.live.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
 import com.live.model.Person;
@@ -11,11 +13,20 @@ public class Manager {
 	public static void main(String[] args) {
 
 		Client client = ClientBuilder.newClient();
-		Response response = client.target("http://localhost:8080/APPPP/webapi/secured/rest").request().get();
+		// Response response =
+		// client.target("http://localhost:8080/APPPP/webapi/secured/rest").request().get();
+		// Person person = response.readEntity(Person.class);
+		// System.out.println(person.getFirstName());
+		//
 
+		WebTarget target = client.target("http://localhost:8080/APPPP/webapi/secured/rest");
+		Builder builder = target.request();
+		Response response = builder.get();
 		Person person = response.readEntity(Person.class);
-
 		System.out.println(person.getFirstName());
+		System.out.println(person.getLastName());
+		
+
 	}
 
 }
