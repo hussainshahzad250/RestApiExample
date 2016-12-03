eb28528bc7fb981e1a7d58ce1aa629ad0fc604b5
Shahzad <Shahzad@192.168.0.2>
2016-12-03 23:01:09 +0530

    Commit on 3 Dec New

--- APPPP/src/main/java/com/live/APPPP/MyResource.java
+++ APPPP/src/main/java/com/live/APPPP/MyResource.java
@@ -1,11 +1,18 @@
 package com.live.APPPP;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.Response.Status;
+
+import org.glassfish.jersey.server.Uri;
 
 import com.live.model.Person;
 
@@ -19,37 +26,61 @@ public class MyResource {
 		return "Got it!";
 	}
 
-//	@Path("/rest/{firstName}/{lastName}")
-//	@GET
-//	@Produces(MediaType.TEXT_PLAIN)
-//	public Person getData(@PathParam("firstName") String firstName, @PathParam("lastName") String lastName) {
-//		
-//		Person person = new Person();
-//		person.setFirstName(firstName);
-//		person.setLastName(lastName);
-//		return person;
-//	}
-	
+	// @Path("/rest/{firstName}/{lastName}")
+	// @GET
+	// @Produces(MediaType.TEXT_PLAIN)
+	// public Person getData(@PathParam("firstName") String firstName,
+	// @PathParam("lastName") String lastName) {
+	//
+	// Person person = new Person();
+	// person.setFirstName(firstName);
+	// person.setLastName(lastName);
+	// return person;
+	// }
+
 	@Path("/rest")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Person getData() {
-		
+
 		Person person = new Person();
 		person.setFirstName("Shahzad");
 		person.setLastName("Hussain");
 		return person;
 	}
-	
-	
+
 	@Path("/rest1")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getdata() {
-		
+
 		Person person = new Person();
 		person.setFirstName("Shahzad");
 		person.setLastName("Hussain");
 		return Response.status(Status.CREATED).entity(person).build();
+	}
+
+	@Path("/rest2")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response data() throws URISyntaxException {
+
+		Person person = new Person();
+		person.setFirstName("Shahzad");
+		person.setLastName("Hussain");
+		return Response.created(new URI("/webapi/secured/rest" + person.getFirstName())).entity(person).build();
+	}
+
+	@Path("/rest3")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response data1(@Context UriInfo uriInfo) throws URISyntaxException {
+		System.out.println(uriInfo.getAbsolutePath());
+		Person person = new Person();
+		person.setFirstName("Shahzad");
+		person.setLastName("Hussain");
+		String firstname = person.getFirstName();
+		URI uri = uriInfo.getAbsolutePathBuilder().path(firstname).build();
+		return Response.created(uri).entity(person).build();
 	}
 }
